1d45eeca23d499181e8c0ae0ae87ea3dcec4913c
Tirth Patel <tirthppatelofficial@gmail.com>
2023-01-29 01:40:11 -0500

    profile

---
+++ After_Login/profile/fetchprofile.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCHX2s435RFvrZ72AHNzFpctBIvIrqnnT4",
+    authDomain: "gratler-cec3b.firebaseapp.com",
+    databaseURL: "https://gratler-cec3b-default-rtdb.firebaseio.com",
+    projectId: "gratler-cec3b",
+    storageBucket: "gratler-cec3b.appspot.com",
+    messagingSenderId: "1028782255982",
+    appId: "1:1028782255982:web:ded319324feeb4a148db7f",
+    measurementId: "G-FNBN7QRX38"
+  };
+  firebase.initializeApp(firebaseConfig);
