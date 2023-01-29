5ba4b1d91ae3201d6be23bc397ea36489faa5823
Tirth Patel <tirthppatelofficial@gmail.com>
2023-01-29 02:23:52 -0500

    Tcommit

--- After_Login/profile/fetchprofile.js
+++ After_Login/profile/fetchprofile.js
@@ -9,8 +9,29 @@ const firebaseConfig = {
     measurementId: "G-FNBN7QRX38"
   };
   firebase.initializeApp(firebaseConfig);
+  var userl2= localStorage.getItem("userlog");
+    const val3 = '_G';
+    var t2=userl2.substring(0, 5);
+    var logged_u2=t2+val3;
+    console.log(logged_u2);
   let id2 = "signup";
+  firebase.database().ref('login/'+ id2).orderByChild('username').equalTo('toxicofficials').on("value", function(snapshot) {
+    console.log(snapshot.val());
+    snapshot.forEach(function(data) {
+        console.log(data.key);
+        
+    });
+});
+//   firebase.database().ref('login/'+ id2).on('value',(snap)=>{
+//     console.log(snap.val());
+//   });
+// firebase.database().ref('login/'+ id2).once('value',   function(snapshot) {
+//     snapshot.forEach(function(childSnapshot) {
+//       var childKey = childSnapshot.key;
+//       var childData = childSnapshot.val();
+//       console.log(childKey);
+//       console.log(childData);
+//       // ...
+//     });
+//   });
   
-  firebase.database().ref('login/'+ id2).on('value',(snap)=>{
-    console.log(snap.val());
-  });
